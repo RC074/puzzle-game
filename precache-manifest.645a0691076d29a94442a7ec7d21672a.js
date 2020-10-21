ea268e0c1953c88b9798ef243318da6d4f3e19b9
binarflow <clouds.overflow@gmail.com>
2020-10-21 17:36:15 -0400

    Updates

---
+++ precache-manifest.645a0691076d29a94442a7ec7d21672a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f336ac90454a1ac0f4091a8356ec07",
+    "url": "/puzzle-game/index.html"
+  },
+  {
+    "revision": "671c77397ca1f1886b3c",
+    "url": "/puzzle-game/static/css/main.0dec4276.chunk.css"
+  },
+  {
+    "revision": "68472067adce8492c1d7",
+    "url": "/puzzle-game/static/js/2.d8a4e8cd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/puzzle-game/static/js/2.d8a4e8cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "671c77397ca1f1886b3c",
+    "url": "/puzzle-game/static/js/main.763733aa.chunk.js"
+  },
+  {
+    "revision": "f954fb5c2cb6b47375c5",
+    "url": "/puzzle-game/static/js/runtime-main.a70e58f3.js"
+  }
+]);
